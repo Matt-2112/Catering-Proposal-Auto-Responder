8cd1c5c15b72b731fe2dd7cac94234b913aefd01
Matt-2112 <mattconnell2112@gmail.com>
2023-08-22 14:26:26 -0700

    removed dev logs

--- src/controllers/GPTController.js
+++ src/controllers/GPTController.js
@@ -9,7 +9,7 @@ GPTController.generateResponse = async (req, res, next) => {
     let addInfo = req.body.additonalInfo
     const prompt = `im having a party for ${numPeople} people give me a sample menu with ${numApps} appetizers, ${numMains} mains and ${numSalads} salad. ${addInfo}.`;
     const completion = await configer(prompt);
-    console.log(completion.data.choices[0].message.content);
+    // console.log(completion.data.choices[0].message.content);
     res.locals.answer = completion.data.choices[0].message.content;
     return next();
 };
